3e850f1a3f5f2aa26f3b92d160b9b48df3f606e3
y <145238134+yynill@users.noreply.github.com>
2023-10-09 10:40:55 +0200

    drag and drop gates
    Drag and drop gates from menu to screen.
    
    Drag and drop from screen to screen.

--- backup main.py
+++ backup main.py
@@ -53,75 +53,54 @@ class Gate:
         self.y = y
 
 
+def delete_gate_by_id(gate_id):
+    for gate in gates:
+        if gate.id == gate_id:
+            gates.remove(gate)
+
+
 def check_menu_click(mouse_x, mouse_y):
     if 0 < mouse_y < 10+SYMBOL_HEIGHT+10:
         # AND-GATE
-        new_gate_type = None
         if 0*(SYMBOL_WIDTH+10) + 10 < mouse_x < (0*(SYMBOL_WIDTH+10) + 10) + SYMBOL_WIDTH:
-            new_gate_type = 'AND_GATE'
+            return 'AND_GATE'
         # NOT-GATE
         if 1*(SYMBOL_WIDTH+10) + 10 < mouse_x < (1*(SYMBOL_WIDTH+10) + 10) + SYMBOL_WIDTH:
-            new_gate_type = 'NOT_GATE'
+            return 'NOT_GATE'
         # OR-GATE
         if 2*(SYMBOL_WIDTH+10) + 10 < mouse_x < (2*(SYMBOL_WIDTH+10) + 10) + SYMBOL_WIDTH:
-            new_gate_type = 'OR_GATE'
+            return 'OR_GATE'
         # NAND-GATE
         if 3*(SYMBOL_WIDTH+10) + 10 < mouse_x < (3*(SYMBOL_WIDTH+10) + 10) + SYMBOL_WIDTH:
-            new_gate_type = 'NAND_GATE'
+            return 'NAND_GATE'
         # NOR-GATE
         if 4*(SYMBOL_WIDTH+10) + 10 < mouse_x < (4*(SYMBOL_WIDTH+10) + 10) + SYMBOL_WIDTH:
-            new_gate_type = 'NOR_GATE'
-
-        print(new_gate_type)
-        if new_gate_type is not None:
-            new_gate = Gate(len(gates), new_gate_type,
-                            gate_images[new_gate_type], mouse_x, mouse_y)
-
-            clicked_gate = new_gate.id
-            gates.append(new_gate)
-            new_gate_type = None
-            return clicked_gate
-
-
-def get_existing_gate(mouse_x, mouse_y, gates):
-    for gate in gates:
-        if gate.x < mouse_x < gate.x + SYMBOL_WIDTH and gate.y < mouse_y < gate.y + SYMBOL_HEIGHT:
-            return gate.id
-
-
-def drag_screen(mouse_x, mouse_y, gates):
-    pass
-
-
-def get_gate_by_id(gate_id):
-    for gate in gates:
-        if gate.id == gate_id:
-            return gate
-    return None
+            return 'NOR_GATE'
 
 
 def draw_window(menu_bar, and_gate_btn, not_gate_btn, or_gate_btn,
                 nand_gate_btn, nor_gate_btn, gates):
     window.fill(BACKGROUND_COLOR)
 
-    for gate in gates:
-        window.blit(gate.image, (gate.x, gate.y))
     window.blit(menu_bar, (0, 0))
-
     window.blit(AND_GATE, (and_gate_btn.x, and_gate_btn.y))
     window.blit(NOT_GATE, (not_gate_btn.x, not_gate_btn.y))
     window.blit(OR_GATE, (or_gate_btn.x, or_gate_btn.y))
     window.blit(NAND_GATE, (nand_gate_btn.x, nand_gate_btn.y))
     window.blit(NOR_GATE, (nor_gate_btn.x, nor_gate_btn.y))
 
+    for gate in gates:
+        window.blit(gate.image, (gate.x, gate.y))
     pygame.display.update()
 
 
 def main():
-    global dragging_gate
-    dragging_gate = None
+    program_state = "create"
+    # create
+    # drag
+    # move (screen)
 
-    menu_bar = pygame.Surface((WIDTH, 10+SYMBOL_HEIGHT+10))
+    menu_bar = pygame.Surface(((SYMBOL_WIDTH+10)*5+10, 10+SYMBOL_HEIGHT+10))
     menu_bar.fill((128, 128, 128))
     and_gate_btn = pygame.Rect(
         0*(SYMBOL_WIDTH+10) + 10, 10, SYMBOL_WIDTH, SYMBOL_HEIGHT)
@@ -144,37 +123,21 @@ def main():
             if event.type == pygame.QUIT:
                 run = False
 
-            # left click
             if event.type == pygame.MOUSEBUTTONDOWN:
                 if event.button == 1:  # left mouse (button 1)
                     mouse_x, mouse_y = event.pos
 
-                    clicked_gate_id = check_menu_click(mouse_x, mouse_y)
-                    if clicked_gate_id is not None:
-                        dragging_gate = get_gate_by_id(clicked_gate_id)
-                    elif clicked_gate_id is None:
-                        this_gate = get_existing_gate(mouse_x, mouse_y, gates)
-                        dragging_gate = get_gate_by_id(this_gate)
-                    else:  # drag whole screen
-                        drag_screen(mouse_x, mouse_y, gates)
+                    new_gate_type = check_menu_click(mouse_x, mouse_y)
+                    print(new_gate_type)
 
-            # drag mouse
-            if event.type == pygame.MOUSEMOTION:
-                if dragging_gate != None:
-                    mouse_x, mouse_y = event.pos
-                    dragging_gate.x = mouse_x - SYMBOL_WIDTH // 2
-                    dragging_gate.y = mouse_y - SYMBOL_HEIGHT // 2
-                else:
-                    pass
+                    if new_gate_type is not None:
+                        new_gate = Gate(len(gates), new_gate_type,
+                                        gate_images[new_gate_type], mouse_x, mouse_y)
 
-            # release gate
-            if event.type == pygame.MOUSEBUTTONUP:
-                if event.button == 1 and dragging_gate is not None:
-                    if dragging_gate.y > SYMBOL_HEIGHT:
-                        # Release the gate by setting dragging_gate to None
-                        dragging_gate = None
-                else:
-                    gates.remove(dragging_gate)
+                        gates.append(new_gate)
+                        new_gate_type = None
+
+                        # clicked_gate = new_gate
 
         draw_window(menu_bar, and_gate_btn, not_gate_btn, or_gate_btn,
                     nand_gate_btn, nor_gate_btn, gates)
